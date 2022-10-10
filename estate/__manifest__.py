6b43cea27f7e75e52bc4b36570dd380337841043
Lukas Kaluscha <lukas.kaluscha@jobrad.org>
2022-10-10 11:40:22 +0200

    Chapter 5 and 6
    Adding security and action view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,9 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_views.xml'
     ],
     "installable": True,
     'license': 'LGPL-3',
