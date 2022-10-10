2133ddf9f2f16f4dd52393a8d0a69d0ab6fb31f8
Lukas Kaluscha <lukas.kaluscha@jobrad.org>
2022-10-10 10:58:04 +0200

    Chapter 4
    Adding fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,23 @@
-from odoo import models
+from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
+    _description = "Some information about estate properties"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(string='Garden Orientation',
+                                          selection=[('north', 'North'), ('sout', 'South'), ('west', 'West'),
+                                                     ('east', 'East')])
