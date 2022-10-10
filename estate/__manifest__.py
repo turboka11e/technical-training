7a8b6e7b0def1b0f011aeb88aa27213cb00a5633
Lukas Kaluscha <lukas.kaluscha@jobrad.org>
2022-10-10 17:01:15 +0200

    Chapter 8
    Added property type menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,10 +7,12 @@
         # Security
         'security/ir.model.access.csv',
         # Views
-        'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menus.xml'
     ],
     "installable": True,
     'license': 'LGPL-3',
 }
+
+
