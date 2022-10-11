154528df817d34677b16c2336836321deee05025
Lukas Kaluscha <lukas.kaluscha@jobrad.org>
2022-10-11 13:56:32 +0200

    Chapter 14
    Linking to other modules

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -10,10 +10,10 @@ class EstatePropertyType(models.Model):
 
     property_ids = fields.One2many("estate.property", "property_type_id")
 
-    # offer_ids = fields.One2many("estate.property.offer", "property_type_id")
-    # offer_count = fields.Integer(compute="_compute_count_offers")
-    #
-    # @api.depends("offer_ids")
-    # def _compute_count_offers(self):
-    #     for record in self:
-    #         record.offer_count = len(record.offer_ids)
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_count_offers")
+
+    @api.depends("offer_ids")
+    def _compute_count_offers(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
