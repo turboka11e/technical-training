4be1bbc5e1801fbac286ab88aa206ef4802af3d9
Lukas Kaluscha <lukas.kaluscha@jobrad.org>
2022-10-11 11:18:27 +0200

    Chapter 13
    Adding inheritance

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    property_ids = fields.One2many("estate.property", "salesperson",
+    property_ids = fields.One2many("estate.property", "salesperson_id",
                                    domain="('state', 'in', ['offer received', 'offer accepted'])")
